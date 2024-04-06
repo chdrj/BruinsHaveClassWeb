b3f50ed94f7c680297f6808d5d46e83e7efd664e
chdrj <chdrj@g.ucla.edu>
2024-04-06 14:19:03 -0700

    firebase changed

--- website/src/firebase/config.js
+++ website/src/firebase/config.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCDC6_Abv-2YD-Nv2gogakE8V2PhJCXIDY",
+  authDomain: "bruinhaveclass.firebaseapp.com",
+  databaseURL: "https://bruinhaveclass-default-rtdb.firebaseio.com",
+  projectId: "bruinhaveclass",
+  storageBucket: "bruinhaveclass.appspot.com",
+  messagingSenderId: "6199150270",
+  appId: "1:6199150270:web:fd3198a9c7d7e0a54480ef",
+  measurementId: "G-3WB2VGJF7G"
 };
 
 // Initialize Firebase
-let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-
-export default firebase_app;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
